5f83f6d241a0eff44fd9d536bfd0952f92931148
Dane Schmidt <daneschmidt@gmail.com>
2020-01-31 21:00:35 -0600

    setup Company class and file basics

---
+++ app.ts
@@ -0,0 +1,14 @@
+import express = require('express');
+import { Company } from "./server/modules/Company"
+
+const app: express.Application = express();
+
+const company = new Company();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port: 3000')
+});
