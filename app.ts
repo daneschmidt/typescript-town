abcf46cd65d3792325e600c52530d63dcb866811
Dane Schmidt <daneschmidt@gmail.com>
2020-01-31 21:34:09 -0600

    got an intervel setup and running

--- app.ts
+++ app.ts
@@ -4,11 +4,12 @@ import { Town } from "./server/modules/Town"
 const app: express.Application = express();
 
 const town = new Town();
+town.init();
 
 app.get('/', (req: express.Request, res: express.Response) => {
     res.send('Hello World');
 });
 
 app.listen(3000, () => {
-    console.log('Example app listening on port: 3000')
+    console.log('We ride on Port: 3000')
 });
--- server/modules/Town.ts
+++ server/modules/Town.ts
@@ -1,5 +1,13 @@
 export class Town {
-    constructor(){
+    constructor() { }
+
+    init() {
         console.log("Welcome to TypeScript Town!");
+
+        setInterval(this.onTimerInterval, 1000);
+    }
+
+    onTimerInterval() {
+        console.log("Its a CRAZY Town!");
     }
 }
