1f7fa4703261f0e177aa6415ca0961d006730808
Dane Schmidt <daneschmidt@gmail.com>
2020-02-01 19:42:08 -0600

    setting up to start creating random citizens

--- server/modules/Town.ts
+++ server/modules/Town.ts
@@ -1,27 +1,45 @@
-import {Citizen} from "./Citizen";
+import { Citizen } from "./Citizen";
 
 export class Town {
     private timer: Object = {};
-    private timerCount: number = 0;
+    private timerCount: number;
+    private citizens: Citizen[];
 
-    constructor() { }
+    constructor() {
+        this.timerCount = 0;
+        this.citizens = [];
+    }
 
-    public init() {
+    public init(): void {
         console.log("Welcome to TypeScript Town!");
         // .bind allows me to avoid maintain THIS in the scope correctly for the onTimerInterval
         this.timer = setInterval(this.onTimerInterval.bind(this), 1000);
-        const dane = new Citizen("dane", "schmidt", "engineer", 10000000, "hack the planet");  
+        this.createCitizen();
+
+        while(this.citizens.length < 10) {
+            this.createCitizen();
+        }
+
+        console.log(this.citizens);
+
+        
     }
 
-    private onTimerInterval() {
+    private onTimerInterval(): void {
         console.log("Its a CRAZY Town!");
         this.timerCount++;
         this.randomEvent();
         console.log(this.timerCount);
     }
 
-    private randomEvent() {
+    private randomEvent(): void {
         //some random event happens here
+    }
+
+    private createCitizen():Citizen {
+        const newCitizen = new Citizen("dane", "schmidt", "engineer", 10000000, "hack the planet");
+        this.citizens.push(newCitizen);
+        return newCitizen;
     }
 
 }
