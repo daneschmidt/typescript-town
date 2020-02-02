cb7a86696d6bc9992fd6da45d91e5ea1073536a2
Dane Schmidt <daneschmidt@gmail.com>
2020-02-01 19:04:30 -0600

    timer setup

--- server/modules/Town.ts
+++ server/modules/Town.ts
@@ -1,13 +1,18 @@
 export class Town {
+    timer: Object = {};
+    timerCount: number = 0;
+
     constructor() { }
 
     init() {
         console.log("Welcome to TypeScript Town!");
-
-        setInterval(this.onTimerInterval, 1000);
+            // .bind allows me to avoid maintan THIS in the scope correctly for the onTimerInterval
+        this.timer = setInterval(this.onTimerInterval.bind(this), 1000);
     }
 
     onTimerInterval() {
         console.log("Its a CRAZY Town!");
+        this.timerCount++;
+        console.log(this.timerCount);
     }
 }
